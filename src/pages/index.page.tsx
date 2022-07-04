a2ab7b0c180a2a042d41d32445cb9377b1e621e9
PaulHaze <dev@artemist.com.au>
2022-07-05 09:24:34 +1000

    feat: add layout to home and about

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,24 +1,28 @@
 /* eslint-disable react/function-component-definition */
-import type { NextPage } from 'next';
-import Head from 'next/head';
+// import type { NextPage } from 'next';
+// import Head from 'next/head';
+import { MainLayout, SidebarLayout } from '@Layouts';
 import { CatCard } from '@Components';
 import { mockCatCardProps } from '@/components/cards/CatCard.mocks';
 
-const Home: NextPage = () => {
+import { NextPageWithLayout } from '@/types/page';
+
+const Home: NextPageWithLayout = () => {
   return (
-    <div>
-      <Head>
-        <title>NextJs Enterprise Template</title>
-        <link rel="icon" href="/favicon.png" />
-        <link rel="apple-touch-icon" href="/logo192.png" />
-      </Head>
-
-      <div className="bg-red-500 h-[64px]">nav bar</div>
-
+    <section>
       <div className="flex flex-col min-h-[calc(100vh-64px)] bg-gradient-to-br from-green-300/50 to-sky-800/50 items-center justify-center px-4">
         <CatCard {...mockCatCardProps.base} />
       </div>
-    </div>
+    </section>
+  );
+};
+
+Home.getLayout = (page) => {
+  return (
+    <MainLayout>
+      <SidebarLayout />
+      {page}
+    </MainLayout>
   );
 };
 
