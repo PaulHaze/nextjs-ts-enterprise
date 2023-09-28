7eb98f5bb3e853aae911eb3ae0f595d29100c3e1
PaulHaze <dev@artemist.com.au>
2023-09-29 07:20:32 +1000

    chore: version bumps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
     >
       <div className="h-[100vh] bg-gradient-to-br from-green-300/50 to-sky-800/50 flex items-center justify-center px-5">
         <div className="flex flex-col justify-center bg-white rounded-lg shadow-2xl p-md sm:p-lg max-w-md">
-          <h1 className="font-bold text-center text-transparent bg-clip-text bg-gradient-to-b from-indigo-400 to-violet-800">
+          <h1 className="font-bold text-black text-center text-transparent bg-clip-text m-10">
             Im your new Tailwind Project!
           </h1>
           <p className="text-[#504e63] text-center">
