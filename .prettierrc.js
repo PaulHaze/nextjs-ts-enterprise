423f3aa5b9af83cac9ce58b0929775a5699076ea
PaulHaze <dev@artemist.com.au>
2023-09-29 08:08:41 +1000

    fix: prettier and tailwind plugin

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  plugins: ['prettier-plugin-tailwindcss'],
   semi: true,
   trailingComma: 'all',
   singleQuote: true,
   printWidth: 80,
   tabWidth: 2,
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+
 import { Meta, MainLayout } from '@/layouts';
 
 const Home: NextPage = () => {
@@ -11,9 +12,9 @@ const Home: NextPage = () => {
         />
       }
     >
-      <div className="mt-3 flex h-[100vh] items-center justify-center bg-gradient-to-br from-green-300/50 to-sky-800/50 p-4 px-5">
-        <div className="mt-10 flex max-w-md flex-col justify-center rounded-lg bg-white shadow-2xl sm:p-lg">
-          <h1 className="bg-clip-text text-center font-bold text-black">
+      <div className="mt-3 flex h-[100vh] items-center  justify-center bg-gradient-to-br from-green-300/50 to-sky-800/50 p-4 px-5">
+        <div className="mt-10 flex max-w-md flex-col justify-center rounded-lg bg-white px-3 shadow-2xl sm:p-lg">
+          <h1 className="bg-clip-text text-center font-bold text-black ">
             Im your new Tailwind Project...
           </h1>
           <p className="text-center text-[#504e63]">
