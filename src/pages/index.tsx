dee7c402c5e4efa1ff8eb510ddb9db18760bd068
PaulHaze <dev@artemist.com.au>
2023-09-29 07:26:52 +1000

    chore: add prettier tailwind plugin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,8 @@ const Home: NextPage = () => {
     >
       <div className="h-[100vh] bg-gradient-to-br from-green-300/50 to-sky-800/50 flex items-center justify-center px-5">
         <div className="flex flex-col justify-center bg-white rounded-lg shadow-2xl p-md sm:p-lg max-w-md">
-          <h1 className="font-bold text-black text-center text-transparent bg-clip-text m-10">
-            Im your new Tailwind Project!
+          <h1 className="font-bold text-black text-center bg-clip-text">
+            Im your new Tailwind Project...
           </h1>
           <p className="text-[#504e63] text-center">
             Delete this and make something great!
