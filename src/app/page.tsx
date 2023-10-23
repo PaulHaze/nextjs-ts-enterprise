d189e4aa25724b66fa653e0301633223acc75dec
PaulHaze <dev@artemist.com.au>
2023-10-23 12:10:38 +1100

    chore: remove uneeded h4 from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <div className="flex-center h-screen bg-gray-300">
       <h4>Barebones TW Enterprise Next Starter</h4>
-      <h5>Redploy trigger for vercel</h5>
     </div>
   );
 }
