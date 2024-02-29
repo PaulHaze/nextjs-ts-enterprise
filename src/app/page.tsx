84f3b15e167e17d781854dd22e08463225d60b1b
PaulHaze <dev@artemist.com.au>
2024-03-01 07:40:06 +1100

    chore: style changes to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex-center min-h-screen">
-      <h4>Barebones TW Enterprise Next Starter</h4>
+    <div className="flex-center min-h-screen flex-col">
+      <h1>Barebones TW Enterprise Next Starter</h1>
+      <p>Delete this and build something</p>
     </div>
   );
 }
