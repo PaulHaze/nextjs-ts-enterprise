95cd035fcc46b046d77448641ddcb3d076b048b0
PaulHaze <dev@artemist.com.au>
2024-03-01 07:21:49 +1100

    feat: add dark styles to global scss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex-center h-screen bg-gray-300">
+    <div className="flex-center min-h-screen">
       <h4>Barebones TW Enterprise Next Starter</h4>
     </div>
   );
